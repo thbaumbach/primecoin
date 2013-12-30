b1904411cf1244cafb3462fcd1ed5edf7ab673f8
Mikael Hirki <mikaelh@mbnet.fi>
2013-12-30 23:29:23 +0200

    Remove chainspermin and primespersec from the output of 'getmininginfo' because they are not useful metrics anymore.

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -142,7 +142,6 @@ Value getmininginfo(const Array& params, bool fHelp)
 
     Object obj;
     obj.push_back(Pair("blocks",        (int)nBestHeight));
-    obj.push_back(Pair("chainspermin",  getchainspermin(params, false)));
     obj.push_back(Pair("chainsperday",  dChainsPerDay));
     obj.push_back(Pair("currentblocksize",(uint64_t)nLastBlockSize));
     obj.push_back(Pair("currentblocktx",(uint64_t)nLastBlockTx));
@@ -150,7 +149,6 @@ Value getmininginfo(const Array& params, bool fHelp)
     obj.push_back(Pair("errors",        GetWarnings("statusbar")));
     obj.push_back(Pair("generate",      GetBoolArg("-gen")));
     obj.push_back(Pair("genproclimit",  (int)GetArg("-genproclimit", -1)));
-    obj.push_back(Pair("primespersec",  getprimespersec(params, false)));
     obj.push_back(Pair("pooledtx",      (uint64_t)mempool.size()));
     obj.push_back(Pair("sieveextensions",(int)nSieveExtensions));
     obj.push_back(Pair("sievepercentage",(int)nSievePercentage));
