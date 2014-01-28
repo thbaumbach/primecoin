bd0363396f7d93cbfd48caaa1ddc5428d4581cf4
Thomas Baumbach <tho.baumbach@googlemail.com>
2014-01-28 21:55:35 +0100

    experimental, DO-NOT-TOUCH merge newest mikaelh with pool miner code

---
+++ src/main_poolminer.hpp
@@ -0,0 +1,35 @@
+#if defined(__MINGW64__)
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+
+#include <boost/thread.hpp>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time_io.hpp>
+#include <cstring>
+
+enum SHAMODE { SPHLIB = 0, AVXSSE4 };
+
+typedef struct {
+  // comments: BYTES <index> + <length>
+  int32_t nVersion;            // 0+4
+  uint8_t hashPrevBlock[32];       // 4+32
+  uint8_t hashMerkleRoot[32];      // 36+32
+  uint32_t  nTime;               // 68+4
+  uint32_t  nBits;               // 72+4
+  uint32_t  nNonce;              // 76+4
+  uint32_t  birthdayA;          // 80+32+4 (uint32_t)
+  uint32_t  birthdayB;          // 84+32+4 (uint32_t)
+  uint8_t   targetShare[32];
+} blockHeader_t;              // = 80+32+8 bytes header (80 default + 8 birthdayA&B + 32 target)
+
+class CBlockProvider {
+public:
+	CBlockProvider() { }
+	~CBlockProvider() { }
+	virtual blockHeader_t* getBlock(unsigned int thread_id, unsigned int last_time, unsigned int counter) = 0;
+	virtual blockHeader_t* getOriginalBlock() = 0;
+	virtual void setBlockTo(blockHeader_t* newblock) = 0;
+	virtual void submitBlock(blockHeader_t* block, unsigned int thread_id) = 0;
+	virtual unsigned int GetAdjustedTimeWithOffset(unsigned int thread_id) = 0;
+};
